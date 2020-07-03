4a49ecb120329145d824056cf67bae18b39e404b
Yaroslav Usenko <usenko.jaroslav@gmail.com>
2020-07-04 00:04:33 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,67 +1,78 @@
 import fs from 'fs';
+import path from 'path';
 import { Telegraf, Markup } from 'telegraf';
 
-const ADMIN_IDS = (process.env.ADMIN_IDS || '').split(',');
+const USERS_PATH = path.join(__dirname, '..', 'data/users.json');
 
-const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
+const keyboard = {
+  zero: Markup
+    .keyboard(['Тут будет клавиатура'])
+    .oneTime()
+    .resize()
+    .extra(),
+};
 
-const users = JSON.parse(fs.readFileSync(`${__dirname}/data/users.json`, 'utf-8'));
+const config = {
+  token: process.env.TELEGRAM_TOKEN,
+  admins: (process.env.ADMIN_IDS || '').split(',').map((id) => Number(id)),
+  users: JSON.parse(fs.readFileSync(USERS_PATH, 'utf-8')),
+};
 
-bot.catch(async (err, ctx) => {
-  console.log(`Ooops, encountered an error for ${ctx.updateType}`, err);
-});
+const bot = new Telegraf(config.token);
+
+const isAdmin = (userId) => config.admins.includes(userId);
+
+const forwardToAdmin = async (ctx) => {
+  const reply = ctx.message.reply_to_message;
+
+  await Promise.all(
+    config.admins
+      .filter((adminId) => ctx.from.id !== adminId && (reply
+        && reply.forward_from && reply.forward_from.id !== adminId))
+      .map((adminId) => ctx.forwardMessage(adminId, ctx.from.id, ctx.message.id)),
+  );
+};
 
 bot.use(async (ctx, next) => {
   if (ctx.update.message.from) {
     const user = ctx.update.message.from;
-    const currentUser = Object.keys(users).find((id) => user.id.toString() === id);
+    const currentUser = Object.keys(config.users).find((id) => user.id.toString() === id);
+
     if (!currentUser) {
-      users[user.id] = user;
-      fs.writeFileSync(`${__dirname}/data/users.json`, JSON.stringify(users, null, 2));
+      config.users[user.id] = user;
+      fs.writeFileSync(USERS_PATH, JSON.stringify(config.users, null, 2));
     }
   }
   return next();
 });
 
-bot.start(async (ctx, next) => {
-  await ctx.reply('Привет 👋');
-  await ctx.reply('Я бот открытого пространства "Амфитеатр"');
-  await ctx.reply('Я создан, чтоб объединить людей под открытым небом 🌿🍃!');
-  await ctx.reply('Я оповещу тебя о новых мероприятиях');
-  await ctx.reply('Покажу, что на них происходит');
-  await ctx.reply('Ну и может мои создатели ко мне еще что-то прикрутят🤔');
-  await ctx.reply('Внизу например клавиатурка появилась (но пока она ничего не делает)', Markup
-    .keyboard([
-      [Markup.callbackButton('Мероприятия', 'Покажи следующие мероприятия'), '😎 Popular'], // Row1 with 2 buttons
-      ['☸ Setting', '📞 Feedback'], // Row2 with 2 buttons
-      ['📢 Ads', '⭐️ Rate us', '👥 Share'], // Row3 with 3 buttons
-    ])
-    .resize()
-    .extra());
-
-  return next();
-});
-
-bot.on('message', async (ctx, next) => {
-  const { chat, message_id: messageId } = ctx.update.message;
-
-  await Promise.all(
-    ADMIN_IDS
-      .filter((admin) => admin !== chat.id.toString())
-      .map((admin) => ctx.forwardMessage(admin, chat.id, messageId)),
-  );
-
-  if (!ADMIN_IDS.includes(chat.id)) {
-    console.log(ctx.update);
+bot.start(async (ctx) => {
+  if (isAdmin(ctx.message.from.id)) {
+    await ctx.reply('Привет 👋');
+    await ctx.reply('Рад тебя видеть, мастер!', keyboard.zero);
   } else {
-    console.log(ctx.update.message);
-    if (ctx.update.message.reply_to_message && ctx.update.message.reply_to_message.forward_from) {
-      const chatId = ctx.update.message.reply_to_message.forward_from.id;
-      ctx.telegram.sendCopy(chatId, ctx.update.message);
-    }
+    await ctx.reply('Привет 👋');
+    await ctx.reply('Я бот открытого пространства "Амфитеатр"');
+    await ctx.reply('Я создан, чтоб объединить людей под открытым небом 🌿🍃!');
+    await ctx.reply('Я оповещу тебя о новых мероприятиях');
+    await ctx.reply('Покажу, что на них происходит');
+    await ctx.reply('Ну и может мои создатели ко мне еще что-то прикрутят🤔', keyboard.zero);
   }
+});
 
-  return next();
+bot.on('message', async (ctx) => {
+  if (ctx.message.reply_to_message
+        && ctx.message.reply_to_message.forward_from
+        && isAdmin(ctx.message.from.id)) {
+    await ctx.telegram.sendCopy(ctx.message.reply_to_message.forward_from.id, ctx.message);
+    await forwardToAdmin(ctx);
+  } else {
+    await forwardToAdmin(ctx);
+  }
+});
+
+bot.catch(async (err, ctx) => {
+  console.log(`Ooops, encountered an error for ${ctx.updateType}`, err);
 });
 
 bot.launch();
